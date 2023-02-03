6ee5907bd97192610a182a3978121fcea4cb455c
Storm <stormforcejj.illamanati@gmail.com>
2023-02-03 23:34:05 +0000

    Experience section and footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Github, Discord } from '@icons-pack/react-simple-icons';
+import { Github, Discord, Nextdotjs, Nodedotjs, ReactJs, Html5, CssThree, Javascript, Tailwindcss, Python, Git, Visualstudiocode } from '@icons-pack/react-simple-icons';
 import {IoLocationSharp} from 'react-icons/io5';
 
 export default function Index() {
@@ -23,8 +23,52 @@ export default function Index() {
                     </li>
                 </ul>
             </div>
-            <h1 className='text-[50px] font-bold text-white'>Hi, I'm Storm 👋</h1>
-            <p className='text-xl'>I'm a 17 year old aspiring developer and student from Wales.</p>
+            <h1 className='text-[50px] font-bold'>Hi, I'm Storm 👋</h1>
+            <p className='text-lg'>I'm a 17 year old aspiring developer and student from Wales. I primarily deal with web technologies, such as NextJS or React, however I am always willing to give something new a try!</p>
+            <h2 className='text-[35px] font-bold pt-5'>Lopsum Imspum</h2>
+            <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, consequatur? Atque iusto cum corrupti dignissimos asperiores quibusdam veniam iure hic inventore modi numquam, magni cupiditate nisi voluptatem quis provident veritatis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure voluptatem voluptate animi quidem qui incidunt, tempora perspiciatis eveniet est aliquam eius provident sit sed ipsa consequatur, debitis quae, recusandae distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero vero reiciendis est, quo tenetur assumenda ducimus hic, modi accusantium natus accusamus unde animi? Laudantium molestias quisquam dolor quae tempore molestiae.</p>
+            <h2 className='text-[35px] font-bold pt-5'>Experience</h2>
+            <div className="mx-10 my-3">
+                <div className="grid grid-cols-5 gap-4 text-lg">
+                    <div className='flex inline'>
+                        <span><Nextdotjs className='inline-flex align-center' color='#fff' size={20}/> NextJS</span>
+                    </div>
+                    <div>
+                        <span><Nodedotjs className='inline-flex align-center' color='#fff' size={20}/> NodeJS</span>
+                    </div>
+                    <div>
+                        <span><ReactJs className='inline-flex align-center' color='#fff' size={20}/> React</span>
+                    </div>
+                    <div>
+                        <span><Javascript className='inline-flex align-center' color='#fff' size={20}/> JavaScript</span>
+                    </div>
+                    <div>
+                        <span><Tailwindcss className='inline-flex align-center' color='#fff' size={20}/> TailwindCSS</span>
+                    </div>
+                    <div>
+                        <span><Html5 className='inline-flex align-center' color='#fff' size={20}/> HTML</span>
+                    </div>
+                    <div>
+                        <span><CssThree className='inline-flex align-center' color='#fff' size={20}/> CSS</span>
+                    </div>
+                    <div>
+                        <span><Python className='inline-flex align-center' color='#fff' size={20}/> Python</span>
+                    </div>
+                    <div>
+                        <span><Git className='inline-flex align-center' color='#fff' size={20}/> Git</span>
+                    </div>
+                    <div>
+                        <span><Visualstudiocode className='inline-flex align-center' color='#fff' size={20}/> VS Code</span>
+                    </div>
+                </div>
+            </div>
+            <div className="my-7 text-neutral-600">
+                <hr className='border-neutral-600' />
+                <h2 className='text-[35px] font-bold pt-5'>Storm</h2>
+                <p className='text-[20px]'>stormforcejj.co.uk • 2023</p>
+            </div>
+            
+
         </section>
     </div>
     )
